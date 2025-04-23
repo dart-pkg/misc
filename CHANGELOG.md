5fa60fabe7e6ff3d8e5e14c6a1bb54c5287f0d08
JavaCommons Technologies <javacommons@gmail.com>
2025-04-23 19:04:27 +0900

    misc: Modified makeCommandLine()

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 ## 2025.422.1753
 
 - Initial release
+
+## 2025.423.1904
+
+- Modified makeCommandLine()
--- lib/misc.dart
+++ lib/misc.dart
@@ -3,7 +3,7 @@ String makeCommandLine(List<String> commandList) {
   String command = commandList[0];
   for (int i = 1; i < commandList.length; i++) {
     String arg = commandList[i];
-    if (!(arg.startsWith('"') || arg.startsWith("'")) &&
+    if (!(arg.startsWith('"') || arg.startsWith("'") || arg.startsWith('`')) &&
         (commandList[i].contains(' ') || commandList[i].contains(r'\'))) {
       command += ' "${commandList[i]}"';
     } else {
